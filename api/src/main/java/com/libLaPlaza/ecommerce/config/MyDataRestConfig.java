72ca68a19eab0e5212fb06145981fbde15bdf534
Martin Montanari <martinmontanari@pop-os.localdomain>
2023-09-08 15:45:41 -0300

    expose productCategories ids

--- api/src/main/java/com/libLaPlaza/ecommerce/config/MyDataRestConfig.java
+++ api/src/main/java/com/libLaPlaza/ecommerce/config/MyDataRestConfig.java
@@ -1,16 +1,34 @@
 package com.libLaPlaza.ecommerce.config;
 
+import com.libLaPlaza.ecommerce.domain.entities.Product;
+import com.libLaPlaza.ecommerce.domain.entities.ProductCategory;
+
+ import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+/*
 import com.libLaPlaza.ecommerce.domain.entities.Product;
 import com.libLaPlaza.ecommerce.domain.entities.ProductCategory;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     
+    private EntityManager entityManager;
+    
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
         HttpMethod[] theUnsoportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
         config.getExposureConfiguration()
@@ -22,5 +40,73 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
             .forDomainType(ProductCategory.class)
             .withItemExposure((metdata, httMethods) -> httMethods.disable(theUnsoportedActions))
             .withCollectionExposure((metdata, httMethods) -> httMethods.disable(theUnsoportedActions));
+
+        //call an internal helper method
+        exposeIds(config);
     }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+} */
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    // call an internal helper method
+        exposeIds(config);
+    }
+    
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // expose entity ids
+        //
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+
 }
