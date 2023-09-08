e8d064d7d35e74e4fa711acc055e17a1757598fc
Martin Montanari <martinmontanari@pop-os.localdomain>
2023-09-08 17:02:55 -0300

    borro codigo comentado

--- api/src/main/java/com/libLaPlaza/ecommerce/config/MyDataRestConfig.java
+++ api/src/main/java/com/libLaPlaza/ecommerce/config/MyDataRestConfig.java
@@ -3,7 +3,7 @@ package com.libLaPlaza.ecommerce.config;
 import com.libLaPlaza.ecommerce.domain.entities.Product;
 import com.libLaPlaza.ecommerce.domain.entities.ProductCategory;
 
- import java.util.List;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,49 +15,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
-/*
-import com.libLaPlaza.ecommerce.domain.entities.Product;
-import com.libLaPlaza.ecommerce.domain.entities.ProductCategory;
 
-@Configuration
-public class MyDataRestConfig implements RepositoryRestConfigurer {
-    
-    private EntityManager entityManager;
-    
-    @Autowired
-    public MyDataRestConfig(EntityManager theEntityManager) {
-        entityManager = theEntityManager;
-    }
-
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
-        HttpMethod[] theUnsoportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
-        config.getExposureConfiguration()
-            .forDomainType(Product.class)
-            .withItemExposure((metdata, httMethods) -> httMethods.disable(theUnsoportedActions))
-            .withCollectionExposure((metdata, httMethods) -> httMethods.disable(theUnsoportedActions));
-
-        config.getExposureConfiguration()
-            .forDomainType(ProductCategory.class)
-            .withItemExposure((metdata, httMethods) -> httMethods.disable(theUnsoportedActions))
-            .withCollectionExposure((metdata, httMethods) -> httMethods.disable(theUnsoportedActions));
-
-        //call an internal helper method
-        exposeIds(config);
-    }
-
-    private void exposeIds(RepositoryRestConfiguration config){
-        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
-
-        List<Class> entityClasses = new ArrayList<>();
-
-        for(EntityType tempEntityType : entities){
-            entityClasses.add(tempEntityType.getJavaType());
-        }
-
-        Class[] domainTypes = entityClasses.toArray(new Class[0]);
-        config.exposeIdsFor(domainTypes);
-    }
-} */
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
